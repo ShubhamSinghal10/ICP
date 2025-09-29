01456c61034cadc315a3146e5b08a4b18b47860a
ShubhamSinghal10 <shubham.singhal1005@gmail.com>
2025-09-29 10:08:48 +0530

    Update 2. Sliding Window Maximum.java

--- Day 1 (Assignment Sheet)/2. Sliding Window Maximum.java
+++ Day 1 (Assignment Sheet)/2. Sliding Window Maximum.java
@@ -1,4 +1,36 @@
 // 239 leetocode
+
+// t.c = O(N)
+// Optimization 
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        int arr[] = new int[n-k+1];
+
+        Deque<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i < n; i++) {
+
+            while(!q.isEmpty() && q.peekFirst()<=i-k) {
+                q.pollFirst();
+            }
+
+            while(!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+                q.pollLast();
+            }
+            q.add(i);
+            if(i >= k-1) {
+                arr[i-k+1] = nums[q.peekFirst()];
+            }
+        }
+        return arr;
+
+    }
+}
+
+
+
 // Brute force approach
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {

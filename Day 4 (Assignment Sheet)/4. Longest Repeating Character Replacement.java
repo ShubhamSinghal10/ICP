e3db7bb946fada0bbfb68c43bbc4be6d753b4954
ShubhamSinghal10 <shubham.singhal1005@gmail.com>
2025-10-01 15:12:15 +0530

    Update 4. Longest Repeating Character Replacement.java

--- Day 4 (Assignment Sheet)/4. Longest Repeating Character Replacement.java
+++ Day 4 (Assignment Sheet)/4. Longest Repeating Character Replacement.java
@@ -1,1 +1,30 @@
 
+// Optimization 
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        
+
+        int[] freq = new int[26];
+        int l = 0;
+        int r = 0;
+        int maxCount = 0;
+        int maxLen = 0;
+
+        while(r < s.length()) {
+
+            char c = s.charAt(r);
+            freq[c-'A']++;
+
+            maxCount = Math.max(maxCount, freq[c-'A']);
+
+            while( (r-l+1)-maxCount > k) {
+                freq[s.charAt(l)-'A']--;
+                l++;
+            }
+            maxLen = Math.max(maxLen,r-l+1);
+            r++;
+        }
+        return maxLen;
+    }
+}

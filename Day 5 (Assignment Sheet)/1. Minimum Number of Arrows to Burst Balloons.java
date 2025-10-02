8cad375d2b479c09289812b8d94bc31dd8803920
ShubhamSinghal10 <shubham.singhal1005@gmail.com>
2025-10-02 22:50:10 +0530

    Create 1. Minimum Number of Arrows to Burst Balloons.java

---
+++ Day 5 (Assignment Sheet)/1. Minimum Number of Arrows to Burst Balloons.java
@@ -0,0 +1,23 @@
+
+// T.C => O(n log n)
+// S.C => O(1)
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        
+        Arrays.sort(points, (a,b) -> Integer.compare(a[1], b[1]));
+
+        int endpoint = points[0][1];
+
+        int ans = 1;
+
+        for(int i = 1; i < points.length; i++) {
+
+            if(points[i][0] > endpoint) {
+                ans++;
+                endpoint = points[i][1];
+            }
+        }
+        return ans;
+    }
+}

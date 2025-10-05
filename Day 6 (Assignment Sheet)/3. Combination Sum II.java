428401e025fa419f3d1a882301db6a5850caea02
ShubhamSinghal10 <shubham.singhal1005@gmail.com>
2025-10-05 21:19:36 +0530

    Update 3. Combination Sum II.java

--- Day 6 (Assignment Sheet)/3. Combination Sum II.java
+++ Day 6 (Assignment Sheet)/3. Combination Sum II.java
@@ -1,1 +1,34 @@
 
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        List<List<Integer>> list = new ArrayList<>();
+
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        sum(candidates, 0, target, l, list);
+
+        return list;
+    }
+
+    public void sum(int[] arr, int idx, int target, List<Integer> l, List<List<Integer>> list) {
+
+        if(target == 0) {
+            list.add(new ArrayList<>(l));
+            return;
+        }
+
+        for(int i = idx; i < arr.length; i++) {
+
+            if(i > idx && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target) break;
+
+            l.add(arr[i]);
+
+            sum(arr, i+1, target-arr[i], l, list);
+
+            l.remove(l.size()-1);
+        }
+    }
+}
